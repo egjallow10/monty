74807cb3d23576af8339ad9a2dec682b733c09bb
egjallow10 <egjallow10@gmail.com>
2022-09-08 19:50:39 -0700

    proto and the file

--- monty.h
+++ monty.h
@@ -70,3 +70,4 @@ void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
