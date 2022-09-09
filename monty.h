fd8404e6ebe787304c27fb3b01e90fd6c818d25b
egjallow10 <egjallow10@gmail.com>
2022-09-08 19:43:50 -0700

    proto

--- monty.h
+++ monty.h
@@ -68,3 +68,4 @@ void mul(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
